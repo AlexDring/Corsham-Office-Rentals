86c7a91424bc2762e5cd62f9299ee46003f13690
Alex Dring <alexdringppc@gmail.com>
2022-03-16 16:42:52 +0000

    Fix Navigation toggle on mobile

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { useState } from 'react';
 
 export default function Navigation() {
+  const [open, setOpen] = useState();
   return (
     <nav className="bg-red-600">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -53,6 +55,7 @@ export default function Navigation() {
               className="bg-red-900 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={() => setOpen(!open)}
             >
               <span className="sr-only">Open main menu</span>
               {/* <!--
@@ -101,13 +104,19 @@ export default function Navigation() {
       </div>
 
       {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-      <div className="hidden" id="mobile-menu">
+      <div className={open ? 'block' : 'hidden'} id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
           {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-          <Link to="/" className="block nav-item" activeClassName="active">
+          <Link
+            onClick={() => setOpen(!open)}
+            to="/"
+            className="block nav-item"
+            activeClassName="active"
+          >
             Home
           </Link>
           <Link
+            onClick={() => setOpen(!open)}
             to="/offices"
             className="block nav-item"
             activeClassName="active"
@@ -115,6 +124,7 @@ export default function Navigation() {
             Offices
           </Link>
           <Link
+            onClick={() => setOpen(!open)}
             to="/studios"
             className="block nav-item"
             activeClassName="active"
@@ -122,6 +132,7 @@ export default function Navigation() {
             Studios
           </Link>
           <Link
+            onClick={() => setOpen(!open)}
             to="/treatment-rooms"
             className="block nav-item"
             activeClassName="active"
@@ -129,6 +140,7 @@ export default function Navigation() {
             Treatment Rooms
           </Link>
           <Link
+            onClick={() => setOpen(!open)}
             to="/contact"
             className="block nav-item"
             activeClassName="active"
